29b6007310eaf5a289ee1e0487e1b1c0d95b55ff
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2024-03-12 23:16:52 +0300

    manga card

--- src/components/manga-card.tsx
+++ src/components/manga-card.tsx
@@ -1,6 +1,27 @@
+import Image from "next/image";
 import React from "react";
+import { env } from "~/env";
 import { Manga } from "~/types/manga";
 
 export default function MangaCard({ manga }: { manga: Manga }) {
-  return <div>MangaCard</div>;
+  return (
+    <div className="flex flex-col">
+      <div>
+        <Image
+          src={
+            env.NEXT_PUBLIC_MEDIA_PREFIX +
+            manga.attributes.cover.data.attributes.url
+          }
+          width={300}
+          height={400}
+          className="w-full object-cover"
+          alt={manga.attributes.title}
+        />
+      </div>
+
+      <div>
+        <h3 className="text-lg">{manga.attributes.title}</h3>
+      </div>
+    </div>
+  );
 }
