ef612278636a0b96a4443930631557e52028dacd
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2024-03-12 23:30:37 +0300

    manca card placeholder

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,7 @@
 import { unstable_noStore } from "next/cache";
 import Image from "next/image";
 import Link from "next/link";
+import MangaCard from "~/components/cards/manga-card";
 import { env } from "~/env";
 
 import { api } from "~/trpc/server";
@@ -16,42 +17,11 @@ export default async function Home() {
         <div className="container">
           <h1>Mangalar</h1>
 
-          {mangas.data.map((manga, index) => (
-            <div key={index} className="flex flex-col gap-4">
-              <div className="">
-                <Image
-                  src={
-                    env.NEXT_PUBLIC_MEDIA_PREFIX +
-                    manga.attributes.cover.data.attributes.url
-                  }
-                  width={200}
-                  height={400}
-                  alt="test"
-                  className="object-cover"
-                />
-              </div>
-              <div>
-                <h1 className="text-3xl font-black">
-                  {manga.attributes.title}
-                </h1>
-              </div>
-              <div>
-                {manga.attributes.manga_chapters.data.map(
-                  (chapter, chapteridx) => (
-                    <div key={chapteridx}>
-                      <Link
-                        href={`/${manga.attributes.slug}/${chapter.attributes.slug}`}
-                      >
-                        <span className="font-bold">
-                          {chapter.attributes.title}
-                        </span>
-                      </Link>
-                    </div>
-                  ),
-                )}
-              </div>
-            </div>
-          ))}
+          <div className="grid grid-cols-6">
+            {mangas.data.map((manga, index) => (
+              <MangaCard manga={manga} key={index} />
+            ))}
+          </div>
         </div>
       </section>
     </main>
--- src/components/cards/manga-card.tsx
+++ src/components/cards/manga-card.tsx
@@ -2,6 +2,7 @@ import Image from "next/image";
 import React from "react";
 import { env } from "~/env";
 import { Manga } from "~/types/manga";
+import CardPlaceholder from "./card-placeholder.webp";
 
 export default function MangaCard({ manga }: { manga: Manga }) {
   return (
@@ -15,6 +16,8 @@ export default function MangaCard({ manga }: { manga: Manga }) {
           width={300}
           height={400}
           className="w-full object-cover"
+          placeholder="blur"
+          blurDataURL={CardPlaceholder.src}
           alt={manga.attributes.title}
         />
       </div>
